392d8025cbee65315feb49a39720884d69321ad3
Anatoly Sorokin <devilfoxok@gmail.com>
2025-04-06 20:15:55 +0800

    Fix reactImport

--- src/components/PeopleItem.tsx
+++ src/components/PeopleItem.tsx
@@ -1,4 +1,4 @@
-import React, { JSX } from 'react'
+import { JSX } from 'react'
 import { IPeople } from '../App'
 
 interface PeopleItemProps {
--- src/components/PeopleList.tsx
+++ src/components/PeopleList.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import PeopleItem from './PeopleItem'
 import { IPeople } from '../App'
 
